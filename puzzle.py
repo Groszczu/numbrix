edfea77c231738ac1b5649b0c57d71de5d9eb86a
Roch Goszczyński <roch.goszczynski@gmail.com>
2021-04-19 00:46:59 +0200

    Complete solver

--- puzzle.py
+++ puzzle.py
@@ -1,43 +1,41 @@
 import os
-# from pysat.formula import CNF
 import itertools
+import math
+import pysat
 from pysat import solvers
 
 
-def text_to_matrix(input_file):
+def read_board(input_file):
     with open(input_file, 'r') as file:
         values = []
-        rows = file.read().split('\n')
+        rows = file.read()
         id = 1
-        for index, row in enumerate(rows):
-            encoded_row = []
-            for value in row.split():
-                if value == '.':
-                    encoded_value = 0
-                else:
-                    encoded_value = int(value)
-                encoded_row.append((id, encoded_value))
-                id += 1
-            values.append(encoded_row)
+        for value in rows.split():
+            if value == '.':
+                encoded_value = 0
+            else:
+                encoded_value = int(value)
+            values.append((id, encoded_value))
+            id += 1
 
         return values
 
 
 class Puzzle:
     def __init__(self, board):
-        col_len = len(board)
-        for index, row in enumerate(board):
-            row_len = len(row)
-            if row_len != col_len:
-                raise ValueError(
-                    f'Puzzle\'s constructor\'s `board` argument should be two dimensional array with same number of columns and rows. Row with index {index} has length {row_len} and have length {col_len}'
-                )
-
+        n = len(board)
+        
         self.board = board
-        self.rows = col_len
-        self.n = col_len * col_len
+        self.rows = round(math.sqrt(n))
+        self.n = n
         self.all_possible_values = range(1, self.n + 1)
-        self.solver = solvers.Glucose3()
+        self.solver = solvers.Glucose3(with_proof=True)
+
+    def decode_cell(self, encoded_cell):
+        encoded_cell_abs = abs(encoded_cell)
+        id = (encoded_cell_abs - 1) // self.n + 1
+        value = encoded_cell_abs - (id - 1) * self.n
+        return id, value
 
     def encode_cell(self, id, value):
         return (id - 1) * self.n + value
@@ -48,15 +46,22 @@ class Puzzle:
     def cell_has_assigned_value(self, id):
         return [self.encode_cell(id, value) for value in self.all_possible_values]
 
+    def get_cell_value(self, id):
+        for cell in self.board:
+            if cell[0] == id:
+                return cell[1]
+        return None
+
     def cell_is_unique(self, id):
         cnf = []
         for value1 in range(1, self.n):
-            for value2 in range(value1, self.n + 1):
+            for value2 in range(1, self.n):
                 # one cell cannot have two values at once
-                cnf.append([
-                    -self.convert(id, value1),
-                    -self.convert(id, value2)
-                ])
+                if value1 != value2:
+                    cnf.append([
+                        -self.encode_cell(id, value1),
+                        -self.encode_cell(id, value2)
+                    ])
 
         return cnf
 
@@ -82,23 +87,74 @@ class Puzzle:
         return cnf
 
     def get_neighbors_ids(self, id):
+        def in_same_row(id1, id2):
+            res = (id1 - 1) // self.rows == (id2 - 1) // self.rows
+            return res
+        def in_same_col(id1, id2):
+            res = id1 % self.rows == id2 % self.rows
+            return res
+
         neighbors = []
         potential_neighbors = [id - 1, id + 1, id - self.rows, id + self.rows]
         for potential_neighbor_id in potential_neighbors:
-            if potential_neighbor_id > 1 and potential_neighbor_id < self.n:
+            if (potential_neighbor_id >= 1 and
+                potential_neighbor_id <= self.n and
+                (in_same_col(id, potential_neighbor_id) or in_same_row(id, potential_neighbor_id))):
+             
                 neighbors.append(potential_neighbor_id)
 
         return neighbors
 
+    def format_result(self, result):
+        res = ''
+        for id, value in result:
+            res += str(value) + '\t'
+            if (id % self.rows == 0):
+                res += '\n'
+        return res
+
+
     def solve(self):
         for id in self.all_possible_values:
-            self.solver.append_formula(self.cell_has_assigned_value(id))
+            clause = self.cell_has_assigned_value(id);
+            self.solver.add_clause(clause)
         for id in self.all_possible_values:
-            self.solver.append_formula(self.cell_is_unique(id))
-        for r in itertools.product(self.all_possible_values, self.all_possible_values):
-            print(r)
+            for clause in self.cell_is_unique(id):
+                self.solver.add_clause(clause)
+
+        for id1, id2 in itertools.product(self.all_possible_values, self.all_possible_values):
+            if id1 != id2:
+                for clause in self.cells_are_not_equal(id1, id2):
+                    self.solver.add_clause(clause)
+        for id in self.all_possible_values:
+            neighbors = self.get_neighbors_ids(id)
+            for clause in self.cells_neighbor_has_proceding_value(id, neighbors):
+                self.solver.add_clause(clause)
+
+        assumptions = []
+        for id in self.all_possible_values:
+            cell_value = self.get_cell_value(id)
+            if cell_value != 0:
+                assumptions.append(self.encode_cell(id, cell_value))
+        
+        print('Solvable?', self.solver.solve(assumptions))
+        model = self.solver.get_model()
+        self.solver.delete()
+
+        result = []
+        for encoded_cell_value in model:
+            if encoded_cell_value > 0:
+                result.append(self.decode_cell(encoded_cell_value))
+
+        return result
 
 
-input = text_to_matrix('input.txt')
-puzzle = Puzzle(input)
-puzzle.solve()
+
+if __name__ == '__main__':
+    input = read_board('input.txt')
+    puzzle = Puzzle(input)
+    print('Input board')
+    print(puzzle.format_result(puzzle.board))
+    result = puzzle.solve()
+    print('Result board')
+    print(puzzle.format_result(result))
---
+++ tests.py
@@ -0,0 +1,77 @@
+from puzzle import Puzzle
+
+p = Puzzle([
+    (1, 0),
+    (2, 1),
+    (3, 3),
+    (4, 2)
+])
+
+def assert_conntent_equal(L1, L2):
+    assert len(L1) == len(L2) and sorted(L1) == sorted(L2)
+
+def assert_encode_decode(i, v):
+    id, value = p.decode_cell(p.encode_cell(i, v))
+    assert id == i
+    assert value == v
+
+def test_neighbor():
+    assert_conntent_equal(p.get_neighbors_ids(1), [2, 3])
+    assert_conntent_equal(p.get_neighbors_ids(2), [1, 4])
+    assert_conntent_equal(p.get_neighbors_ids(3), [1, 4])
+    assert_conntent_equal(p.get_neighbors_ids(4), [2, 3])
+
+
+def test_assigned_value():
+    assert_conntent_equal(p.cell_has_assigned_value(1), [1, 2, 3, 4])
+    assert_conntent_equal(p.cell_has_assigned_value(2), [5, 6, 7, 8])
+    assert_conntent_equal(p.cell_has_assigned_value(3), [9, 10, 11, 12])
+    assert_conntent_equal(p.cell_has_assigned_value(4), [13, 14, 15, 16])
+
+def test_equal_to():
+    assert_conntent_equal(p.cell_is_equal_to(1, 1), [1])
+    assert_conntent_equal(p.cell_is_equal_to(1, 2), [2])
+    assert_conntent_equal(p.cell_is_equal_to(1, 3), [3])
+    assert_conntent_equal(p.cell_is_equal_to(1, 4), [4])
+
+    assert_conntent_equal(p.cell_is_equal_to(2, 1), [5])
+    assert_conntent_equal(p.cell_is_equal_to(2, 2), [6])
+    assert_conntent_equal(p.cell_is_equal_to(2, 3), [7])
+    assert_conntent_equal(p.cell_is_equal_to(2, 4), [8])
+    
+    assert_conntent_equal(p.cell_is_equal_to(3, 1), [9])
+    assert_conntent_equal(p.cell_is_equal_to(3, 2), [10])
+    assert_conntent_equal(p.cell_is_equal_to(3, 3), [11])
+    assert_conntent_equal(p.cell_is_equal_to(3, 4), [12])
+
+    assert_conntent_equal(p.cell_is_equal_to(4, 1), [13])
+    assert_conntent_equal(p.cell_is_equal_to(4, 2), [14])
+    assert_conntent_equal(p.cell_is_equal_to(4, 3), [15])
+    assert_conntent_equal(p.cell_is_equal_to(4, 4), [16])
+
+def test_get_value():
+    assert p.get_cell_value(1) == 0
+    assert p.get_cell_value(2) == 1
+    assert p.get_cell_value(3) == 3
+    assert p.get_cell_value(4) == 2
+
+def test_encode_decode():
+    assert_encode_decode(1, 1)
+    assert_encode_decode(1, 2)
+    assert_encode_decode(1, 3)
+    assert_encode_decode(1, 4)
+
+    assert_encode_decode(2, 1)
+    assert_encode_decode(2, 2)
+    assert_encode_decode(2, 3)
+    assert_encode_decode(2, 4)
+
+    assert_encode_decode(3, 1)
+    assert_encode_decode(3, 2)
+    assert_encode_decode(3, 3)
+    assert_encode_decode(3, 4)
+
+    assert_encode_decode(4, 1)
+    assert_encode_decode(4, 2)
+    assert_encode_decode(4, 3)
+    assert_encode_decode(4, 4)
